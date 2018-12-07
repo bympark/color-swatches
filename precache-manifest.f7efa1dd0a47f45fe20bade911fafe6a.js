dac816e91bd0687e6a4e083c3c42a50398c627bc
Brian Park <brianympark@gmail.com>
2018-12-07 15:51:36 -0800

    Updates

--- precache-manifest.f7efa1dd0a47f45fe20bade911fafe6a.js
+++ precache-manifest.f7efa1dd0a47f45fe20bade911fafe6a.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/color-swatches/static/js/runtime~main.67e1ceb9.js"
   },
   {
-    "revision": "4739457324b6fbf69a22",
-    "url": "/color-swatches/static/js/main.47394573.chunk.js"
+    "revision": "eb12598b24c395ed62f8",
+    "url": "/color-swatches/static/js/main.eb12598b.chunk.js"
   },
   {
     "revision": "a272833de71efb118cb2",
     "url": "/color-swatches/static/js/1.a272833d.chunk.js"
   },
   {
-    "revision": "4739457324b6fbf69a22",
+    "revision": "eb12598b24c395ed62f8",
     "url": "/color-swatches/static/css/main.08947da1.chunk.css"
   },
   {
-    "revision": "393e1e62c960340e3a7827a981ce2908",
+    "revision": "9e7a5d0f88d3c8400b9c62d92057c3a4",
     "url": "/color-swatches/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/color-swatches/precache-manifest.0cbc4db843de582dee3b4b718dd6e343.js"
+  "/color-swatches/precache-manifest.f7efa1dd0a47f45fe20bade911fafe6a.js"
 );
 
 workbox.clientsClaim();
